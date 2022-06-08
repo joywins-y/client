efbc37d71da2a6c48b6b7b74b75d931e2bcc2d6b
whaijun <1033161981@qq.com>
2022-06-09 01:39:16 +0800

    添加注释 production

--- src/interfaces/production.ts
+++ src/interfaces/production.ts
@@ -1,5 +1,8 @@
 export type Color = 'blue' | 'red' | 'wihet'
 
+/**
+ * 库存一条记录
+ */
 export interface Production {
   /**
    * 产品系列
@@ -24,24 +27,53 @@ export interface Production {
 }
 
 /**
- * 发货记录
+ * 发货记录 一条记录
  */
 export interface PushProduction {
+  /**
+   * 出货时间
+   */
   timer: string
+  /**
+   * 入货数量
+   */
   number: number
+  /**
+   * 入货到达时间
+   */
   dist: string
+  /**
+   * 入货产品
+   */
   production: Production
+  /**
+   * 入货备注
+   */
   description: string
 }
 
 /**
- * 出货记录
+ * 出货记录 一条记录
  */
-
 export interface PopProduction {
+  /**
+   * 出货时间
+   */
   timer: string
+  /**
+   * 出货数量
+   */
   number: number
+  /**
+   * 出货到达时间
+   */
   dist: string
+  /**
+   * 出货产品
+   */
   production: Production
+  /**
+   * 出货备注
+   */
   description: string
 }
